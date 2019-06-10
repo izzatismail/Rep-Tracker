40e619720b8372715a0e588075fcbe421d3a9b56
izzatismail <mizzat1996@gmail.com>
2019-06-10 11:57:04 +0800

    Room and Lifecycle added

---
+++ app/src/main/java/com/izzatismail/reptracker/Persistences/RepRepository.java
@@ -0,0 +1,35 @@
+package com.izzatismail.reptracker.Persistences;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.content.Context;
+
+import com.izzatismail.reptracker.Models.Rep;
+import com.izzatismail.reptracker.Async.insertAsyncTask;
+import com.izzatismail.reptracker.Async.DeleteAsyncTask;
+import com.izzatismail.reptracker.Async.UpdateAsyncTask;
+
+import java.util.List;
+
+public class RepRepository {
+
+    private RepDatabase mRepDatabase;
+
+    public RepRepository(Context context){ mRepDatabase = RepDatabase.getInstance(context); }
+
+    public void insert(Rep rep){
+        new insertAsyncTask(mRepDatabase.getRepDao()).execute(rep);
+    }
+
+    public void update(Rep rep){
+        new UpdateAsyncTask(mRepDatabase.getRepDao()).execute(rep);
+    }
+
+    public void delete(Rep rep){
+        new DeleteAsyncTask(mRepDatabase.getRepDao()).execute(rep);
+    }
+
+    public LiveData<List<Rep>> retrieveRepTask(){
+        return mRepDatabase.getRepDao().getAllRep();
+    }
+}
