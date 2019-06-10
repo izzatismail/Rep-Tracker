85a9e177b0471ae3c1d8ee2a90b963390fb8f456
izzatismail <mizzat1996@gmail.com>
2019-06-10 11:57:28 +0800

    Update UI

--- app/src/main/java/com/izzatismail/reptracker/Fragments/ChartFragment.java
+++ app/src/main/java/com/izzatismail/reptracker/Fragments/ChartFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +13,42 @@ import com.izzatismail.reptracker.R;
 
 public class ChartFragment extends Fragment {
 
+    public static final String TAG = "ChartFragment";
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_chart, container, false);
+    }
+
+    /* Lifecycle */
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart: called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume: called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause: called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop: called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called");
     }
 }
