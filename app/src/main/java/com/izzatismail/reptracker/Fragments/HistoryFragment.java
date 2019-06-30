e4b65c9a57e479189d4d502180b0bba11c640446
izzatismail <mizzat1996@gmail.com>
2019-06-30 10:24:47 +0800

    Added CompactCalendarView

--- app/src/main/java/com/izzatismail/reptracker/Fragments/HistoryFragment.java
+++ app/src/main/java/com/izzatismail/reptracker/Fragments/HistoryFragment.java
@@ -1,26 +1,85 @@
 package com.izzatismail.reptracker.Fragments;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.izzatismail.reptracker.Activities.AddNewRepActivity;
 import com.izzatismail.reptracker.Activities.MainActivity;
+import com.izzatismail.reptracker.Models.Rep;
 import com.izzatismail.reptracker.R;
+import com.izzatismail.reptracker.Util.RepDialog;
+import com.izzatismail.reptracker.Util.RepDialogWithoutDelete;
+import com.izzatismail.reptracker.ViewModels.RepViewModel;
 
-public class HistoryFragment extends Fragment {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
-    public static final String TAG = "HistoryFragment";
+public class HistoryFragment extends Fragment implements CompactCalendarView.CompactCalendarViewListener {
+
+    private static final String TAG = "HistoryFragment";
+    private RepViewModel mViewModel;
+    private CompactCalendarView mCompactCalendarView;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM yyyy", Locale.getDefault());
+    private View rootView;
+    private Long millis;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ((MainActivity)getActivity()).setActionBarTitle("Workout Days");
-        return inflater.inflate(R.layout.fragment_history, container, false);
+        rootView = inflater.inflate(R.layout.fragment_history, container, false);
+
+        mCompactCalendarView = (CompactCalendarView)rootView.findViewById(R.id.calendar_rep);
+
+        ((MainActivity)getActivity()).setActionBarTitle(simpleDateFormat.format(mCompactCalendarView.getFirstDayOfCurrentMonth()));
+
+        mViewModel = ViewModelProviders.of(this).get(RepViewModel.class);
+        mViewModel.getAllReps().observe(this, new Observer<List<Rep>>() {
+            @Override
+            public void onChanged(List<Rep> reps) {
+                for (int i=0; i<reps.size(); i++) {
+
+                    Rep data = reps.get(i);
+
+                    String day = data.getTimestamp().substring(0,2);
+                    String month = data.getTimestamp().substring(3,5);
+                    String year = data.getTimestamp().substring(6,10);
+                    String timestamp = month + "/" + day + "/" + year;
+
+                    try {
+                        millis = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault()).parse(timestamp).getTime();
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+
+                    Event event = new Event(Color.RED, millis, data);
+                    mCompactCalendarView.addEvent(event);
+                }
+            }
+        });
+
+        mCompactCalendarView.setListener(this);
+
+        return rootView;
     }
 
     /* Lifecycle */
@@ -52,5 +111,32 @@ public class HistoryFragment extends Fragment {
     public void onDestroy() {
         super.onDestroy();
         Log.d(TAG, "onDestroy: called");
+    }
+
+    @Override
+    public void onDayClick(Date dateClicked) {
+        List<Event> events = mCompactCalendarView.getEvents(dateClicked);
+        if(!events.isEmpty()){
+            Rep data = (Rep)events.get(0).getData();
+
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            Fragment prev = getFragmentManager().findFragmentByTag("dialog");
+            if (prev != null) {
+                ft.remove(prev);
+            }
+            ft.addToBackStack(null);
+
+            RepDialogWithoutDelete repDialog = RepDialogWithoutDelete.newInstance(data);
+            Log.d(TAG, "onDayClick: Selected Note : " + data.toString());
+
+            repDialog.show(ft, "Rep Dialog");
+        }else{
+            Toast.makeText(getActivity(), "No Recorded Added", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onMonthScroll(Date firstDayOfNewMonth) {
+        ((MainActivity)getActivity()).setActionBarTitle(simpleDateFormat.format(firstDayOfNewMonth));
     }
 }
